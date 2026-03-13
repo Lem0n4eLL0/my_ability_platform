f4fa7f1a2a23248e6568ab529fca106fa6589095
Vladislav <vladislavche@bk.ru>
2026-03-13 20:57:02 +0400

    добавлена архитектура проекта, докерфайл

---
+++ backend/GigAnt/src/test/java/com/example/GigAnt/GigAntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GigAnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigAntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
