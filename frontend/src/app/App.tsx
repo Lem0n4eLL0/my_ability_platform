6d3bb3447d7e8ccf4674f062653010f7cd6889b1
Lem0n4eLL0 <vladislavche@bk.ru>
2026-04-03 15:35:36 +0400

    Реализован Footer

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router';
 import { MainLayout } from '../components/layouts/MainLayout';
 import { Header } from '@/components/layouts/Header';
+import { Footer } from '@/components/layouts/Footer';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout header={<Header />} footer={<div>Footer</div>} />}>
-        <Route index element={<div>Hello, world!</div>}></Route>
+      <Route path="/" element={<MainLayout header={<Header />} footer={<Footer />} />}>
+        <Route
+          index
+          element={<div style={{ height: 'calc(100vh - 80px)' }}>Hello, world!</div>}
+        ></Route>
       </Route>
       <Route path="*" element={<div>Error 404</div>}></Route>
     </Routes>
