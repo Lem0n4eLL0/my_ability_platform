2969ef00213503e46675bb29e7e624ab0afa39a1
Lem0n4eLL0 <vladislavche@bk.ru>
2026-04-04 12:54:43 +0400

    Добавление анимации кнопке

--- frontend/src/components/TryTestButton/TryTestButton.tsx
+++ frontend/src/components/TryTestButton/TryTestButton.tsx
@@ -1,8 +1,10 @@
+import { SyntheticEvent } from 'react';
 import { IMGIcon } from '../shells/IMGIcon';
 import style from './TryTestButton.module.scss';
+import clsx from 'clsx';
 
 interface ITryTestButton {
-  onClick: () => void;
+  onClick: (e: SyntheticEvent<HTMLButtonElement>) => void;
 }
 
 export const TryTestButton = (props: ITryTestButton) => {
@@ -10,7 +12,13 @@ export const TryTestButton = (props: ITryTestButton) => {
   return (
     <button type="button" className={style['button']} onClick={onClick}>
       <span className={style['title']}>Пройти тест</span>
-      <IMGIcon src="src/assets/try_test_arrow.svg" alt="arrow" wrapperClassName={style['arrow']} />
+      <div className={style['arrow__wrapper']}>
+        <IMGIcon
+          src="src/assets/try_test_arrow.svg"
+          alt="arrow"
+          wrapperClassName={clsx(style['arrow'], style['arrow-fast_animation'])}
+        />
+      </div>
     </button>
   );
 };
